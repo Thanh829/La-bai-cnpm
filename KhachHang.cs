80a90ab87d08796e22f0dae6c2030fe12558a1d3
Thanh829 <42971407+Thanh829@users.noreply.github.com>
2020-05-21 12:48:39 +0700

    1

---
+++ KhachHang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopLapTop.Models
+{
+    public class KhachHang
+    {
+        public int ID { get; set; }
+        public string Ten { get; set; }
+        public string SDT { get; set; }
+        public string DiaChi { get; set; }
+        public string Level { get; set; }
+        public ICollection<HoaDon> HoaDons { get; set; }
+    }
+}
